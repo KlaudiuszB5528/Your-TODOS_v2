5328b2852040138b78133886f63fad723a0d0af8
KlaudiuszB5528 <klaudiusz.biegacz@gmail.com>
2022-07-29 14:00:11 +0200

    Fix: browserrouter basename add

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
