ccc2368942424f10e6201bb17b74ff7f55014b77
KlaudiuszB5528 <klaudiusz.biegacz@gmail.com>
2022-08-06 14:12:44 +0200

    fix mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,15 @@ import { Sidebar } from "./components/Sidebar";
 import { TodosContextProvider } from "./context/todos-context";
 
 const App: React.FC = () => {
+  const resizeHandler = () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  };
+
+  resizeHandler();
+
+  window.addEventListener("resize", resizeHandler);
+
   return (
     <TodosContextProvider>
       <Header />
